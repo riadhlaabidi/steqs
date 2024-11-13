4b331447e3742781d3121a31771a09f886746442
riadhlaabidi <riadh.by.laabidi@gmail.com>
2024-11-13 19:14:42 +0100

    Restore cursor position on cancelling the search

--- src/find.c
+++ src/find.c
@@ -1,15 +1,35 @@
 #include <string.h>
 
 #include "editor.h"
+#include "find.h"
 #include "status_bar.h"
 #include "util.h"
 
 void find()
 {
-    char *query = prompt("Search: %s", NULL);
+    int prev_cx = ec.cx;
+    int prev_cy = ec.cy;
+    int prev_row_offset = ec.row_offset;
+    int prev_col_offset = ec.col_offset;
 
-    if (!query)
+    char *query = prompt("Search: %s", find_callback);
+
+    if (query) {
+        FREE(query);
+    } else {
+        // back to cursor position before initiating search
+        ec.cx = prev_cx;
+        ec.cy = prev_cy;
+        ec.row_offset = prev_row_offset;
+        ec.col_offset = prev_col_offset;
+    }
+}
+
+void find_callback(char *query, int key)
+{
+    if (key == '\r' || key == '\x1b') {
         return;
+    }
 
     int i;
     for (i = 0; i < ec.num_trows; ++i) {
@@ -22,6 +42,4 @@ void find()
             break;
         }
     }
-
-    FREE(query);
 }
--- src/find.h
+++ src/find.h
@@ -1,3 +1,12 @@
 #pragma once
 
+/**
+ * Launches a prompt for searching the currently opened file
+ */
 void find(void);
+
+/**
+ * Callback function to search for query match and set the cursor position to
+ * the first result
+ */
+void find_callback(char *query, int key);
