e765fafa5f09bc02d2e7d8fbaffefbb15c666bc7
riadhlaabidi <riadh.by.laabidi@gmail.com>
2024-11-24 17:42:50 +0100

    Navigate matches with arrows & restore cursor pos on no match

--- src/find.c
+++ src/find.c
@@ -2,44 +2,95 @@
 
 #include "editor.h"
 #include "find.h"
+#include "kbd.h"
 #include "status_bar.h"
 #include "util.h"
 
-void find()
+static int cx_before_find;
+static int cy_before_find;
+static int r_offset_before_find;
+static int c_offset_before_find;
+
+void restore_cursor_pos()
 {
-    int prev_cx = ec.cx;
-    int prev_cy = ec.cy;
-    int prev_row_offset = ec.row_offset;
-    int prev_col_offset = ec.col_offset;
 
-    char *query = prompt("Search: %s", find_callback);
-
-    if (query) {
-        FREE(query);
-    } else {
-        // back to cursor position before initiating search
-        ec.cx = prev_cx;
-        ec.cy = prev_cy;
-        ec.row_offset = prev_row_offset;
-        ec.col_offset = prev_col_offset;
-    }
+    ec.cy = cy_before_find;
+    ec.cx = cx_before_find;
+    ec.row_offset = r_offset_before_find;
+    ec.col_offset = c_offset_before_find;
 }
 
 void find_callback(char *query, int key)
 {
+    static int last_match = -1;
+    static int direction = 1;
+    static int matches = 0;
+
     if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        if (!matches && key == '\r') {
+            set_status_msg("No matches found!");
+        }
         return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
     }
 
+    if (last_match == -1) {
+        direction = 1;
+    }
+
+    int current = last_match;
     int i;
+    matches = 0;
+
     for (i = 0; i < ec.num_trows; ++i) {
-        text_row *row = &ec.t_rows[i];
+        current += direction;
+
+        if (current == -1) {
+            current = ec.num_trows - 1;
+        } else if (current == ec.num_trows) {
+            current = 0;
+        }
+
+        text_row *row = &ec.t_rows[current];
         char *match = strstr(row->to_render, query);
+
         if (match) {
-            ec.cy = i;
+            matches++;
+            last_match = current;
+            ec.cy = current;
             ec.cx = row_rx_to_cx(row, match - row->to_render);
             ec.row_offset = ec.num_trows;
             break;
         }
+    }
+
+    if (!matches) {
+        restore_cursor_pos();
+    }
+}
+
+void find()
+{
+    cx_before_find = ec.cx;
+    cy_before_find = ec.cy;
+    r_offset_before_find = ec.row_offset;
+    c_offset_before_find = ec.col_offset;
+
+    char *query = prompt(
+        "Search [ESC: cancel, Arrows: next/prev match, Enter: select): %s",
+        find_callback);
+
+    if (query) {
+        FREE(query);
+    } else {
+        restore_cursor_pos();
     }
 }
--- src/find.h
+++ src/find.h
@@ -4,9 +4,3 @@
  * Launches a prompt for searching the currently opened file
  */
 void find(void);
-
-/**
- * Callback function to search for query match and set the cursor position to
- * the first result
- */
-void find_callback(char *query, int key);
